9f796a6ce09674033c3d31e3137e7970ada1622f
GageWaack <45102148+GageWaack@users.noreply.github.com>
2019-08-29 09:38:00 -0500

    update

---
+++ src/SammysMotto2.java
@@ -0,0 +1,7 @@
+public class SammysMotto2 {
+    public static void main(String[] args){
+        System.out.println("           SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs          ");
+        System.out.println("  SsSsSsSs  Sammy's makes it fun in the sun  SsSsSsSs");
+        System.out.println("           SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsS         ");
+    }
+}
